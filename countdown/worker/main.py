1654217b79c12835ff3ab253811d18329552720f
Vlad Lisov <butukay@gmail.com>
2023-10-19 04:05:23 +0300

    refactored message updater main

--- countdown/worker/main.py
+++ countdown/worker/main.py
@@ -1,40 +1,52 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from countdown.chatbot import bot, bot_event_loop
+
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from countdown.data import users
 
 import asyncio
 
 import logging
+log = logging.getLogger(__name__)
 
-async def get_all_countdowns():
-    all_countdowns = []
 
+async def update_messages_a(only_with_time: bool = False):
     async for user in users.get_users_async_iterable():
+        if user.countdowns:
+            log.info(f"Updating {len(user.countdowns)} countdowns for {user.user_id}")
+
         for countdown in user.countdowns:
-            all_countdowns.append(countdown)
+            if only_with_time:
+                if not countdown.settings.show_time:
+                    continue
 
-    return all_countdowns
-
-def update_messages(only_with_time: bool = False):
-    all_countdowns = asyncio.run(get_all_countdowns())
-
-    for countdown in all_countdowns:
-        if only_with_time:
-            if not countdown.settings.show_time:
-                continue
-
-        try:
-            asyncio.run_coroutine_threadsafe(
-                bot.edit_message_text(
+            try:
+                await bot.edit_message_text(
                     inline_message_id=countdown.inline_message_id,
                     text=countdown.get_message_text(),
                     reply_markup=InlineKeyboardMarkup(inline_keyboard=[
                         [ InlineKeyboardButton(text="Edit", callback_data=f"edit:{countdown.inline_message_id}") ],
                     ])
-                ), bot_event_loop
-            )
-        except Exception as e:
-            logging.error(f"Error while updating message {countdown.inline_message_id}: {e}")
-            continue
+                )
 
+                log.info(f" - Updated message {countdown.inline_message_id}")
+
+            except Exception as e:
+                if "message is not modified" in str(e):
+                    log.info(f" - Message {countdown.inline_message_id} is not modified")
+                elif "MESSAGE_ID_INVALID" in str(e):
+                    log.info(f" - Message {countdown.inline_message_id} is invalid, removing...")
+                    user.countdowns.remove(countdown)
+                else:
+                    log.exception(f"Error while updating message {countdown.inline_message_id}: {e}")
+
+                continue
+
+        await users.save_user(user)
+
+
+def update_messages(only_with_time: bool = False):
+    asyncio.run_coroutine_threadsafe(
+        update_messages_a(only_with_time=only_with_time),
+        bot_event_loop
+    )
